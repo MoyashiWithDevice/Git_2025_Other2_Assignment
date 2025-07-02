fbd1d3fab2c7c6059139838b0ac17c7edad95a58
bababa <pijonsan@gmail.com>
2025-07-02 14:21:15 +0900

    正解・不正解を表示する処理を追加

--- Quiz.js
+++ Quiz.js
@@ -1,4 +1,21 @@
 function displayAnswer() {
-    var answerElement = document.getElementById("answer");
-    answerElement.style.display = "block";
+    const radios = document.getElementsByName('quiz');
+    let selectedValue = '';
+    for (let radio of radios) {
+        if (radio.checked) {
+            selectedValue = radio.value;
+            break;
+        }
+    }
+
+    const answer = document.getElementById('answer');
+    if (selectedValue === '焼きそばパン') {
+        answer.textContent = '正解！食べられないパンは焼きそばパン！';
+    } else if (selectedValue) {
+        answer.textContent = '不正解！もう一度考えてみて！';
+    } else {
+        answer.textContent = '選択肢を選んでください。';
+    }
+    answer.style.display = 'block';
+    
 }
